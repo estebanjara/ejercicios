9c1e299e42b304f0754e91becf48d21e47f34e5a
JD <jd@vizcaya.co>
2015-01-21 19:34:27 -0500

    Ejercicio8 primera versión

---
+++ ejercicio8.rb
@@ -0,0 +1,17 @@
+puts " Es un número primo?"
+numero=gets.chomp.to_i
+
+contar=0
+
+(1...numero).each do |x|
+	if (numero% x ==0)
+		contar+=1
+	else
+	end
+end
+
+if contar != 2
+	puts "#{numero} No es un número primo"
+elsif (numero%1 == 0 && numero%numero == 0)
+	puts "#{numero} Es un número primo"
+end
